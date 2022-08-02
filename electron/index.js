9495f49aec9cfe8c467b14b6f7f9c4f4d7127c63
sase <bs.saase@gmail.com>
2022-08-02 14:32:34 -0400

    added minor css changes

--- electron/index.js
+++ electron/index.js
@@ -4,14 +4,15 @@ const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 400,
-    height: 300,
+    width: 300,
+    height: 50,
     autoHideMenuBar: true
   });
   
   win.loadFile(path.join(__dirname, 'index.html'));
 
   win.on('close', () => {
+    console.log('Trying to kill port 8082');
     exec('kill-port --port 8082');
   });
 };
@@ -19,12 +20,10 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow();
 
-    console.log('Trying to kill port 8082');
   exec('npm run pre-electron');
   exec('lt -p 8082 -s ncex');
 });
 
 app.on('window-all-closed', async() => {
   if (process.platform !== 'darwin') app.quit();
-  await kill(8082, 'tcp');
 });
