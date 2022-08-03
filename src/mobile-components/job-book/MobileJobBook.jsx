f0865e628b919937c91401e0f937689b98340126
sase <bs.saase@gmail.com>
2022-08-03 13:48:33 -0400

    mobile job book init

---
+++ src/mobile-components/job-book/MobileJobBook.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+
+const MobileJobBook = (props) => {
+  
+  return (
+    <div className="mobile-job-book">
+      //
+    </div>
+  );
+};
+
+export default MobileJobBook;
