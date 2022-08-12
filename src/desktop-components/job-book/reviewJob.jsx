f3ed1a41871c51d17d364ff0d26ea2f081b3d0c0
sase <bs.saase@gmail.com>
2022-08-12 09:19:36 -0400

    removed scrap/ccHeaters and added warranty

--- src/desktop-components/job-book/reviewJob.jsx
+++ src/desktop-components/job-book/reviewJob.jsx
@@ -13,13 +13,17 @@ const ReviewJob = props => {
     statorStatus,
     incomingNumber,
     scrap,
-    notes,
     toggleReviewingJob,
     cancelHandler,
     user,
     fetchAndSetJobRange,
     warranty
   } = props;
+
+  const [notes, setNotes] = useState('');
+  const setJobNotes = (e) => {
+    setNotes(e.target.value);
+  };
 
   const [jobNumber, setJobNumber] = useState("AUTO");
 
@@ -46,7 +50,6 @@ const ReviewJob = props => {
         modelNumber,
         serialNumber,
         voltage,
-        ccHeater: parseYesNoValues(ccHeater),
         unloaders,
         statorStatus,
         incomingNumber,
@@ -63,25 +66,6 @@ const ReviewJob = props => {
     .catch(err => err);
   };
 
-  const scrapMap = {
-    YES: {
-      text: 'THIS IS SCRAP',
-      class: 'job-book-review-scrap-text'
-    },
-    NO: {
-      text: 'THIS IS NOT SCRAP',
-      class: 'job-book-review-not-scrap-text'
-    },
-    ['']: {
-      text: '',
-      class: ''
-    },
-    ['?']: {
-      text: '',
-      class: ''
-    }
-  };
-
   return (
     <div className="job-book-review-add-job-container">
       <div className="job-book-review-add-job-text">Review</div>
@@ -94,20 +78,19 @@ const ReviewJob = props => {
       <div className="job-book-review-meta-information">
         <TextField value={voltage} disabled label="Voltage" fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
         <TextField value={unloaders} disabled label="Unloaders" fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
-        <TextField value={ccHeater} disabled label="CC Heaters" fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
+        <TextField value={warranty} disabled label="Warranty" fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
         <TextField value={statorStatus} disabled label="Stator Status" fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
       </div>
 
       <div className="job-book-review-additional-data">
         <div className="job-book-review-notes-container">
-          <TextField value={notes} disabled label="Notes" rows="6" multiline fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
+          <TextField onKeyUp={setJobNotes} label="Notes" rows="6" multiline fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
         </div>
 
         <div className="job-book-review-identification-numbers-x-scrap-status">
           <TextField value={incomingNumber} disabled label="Incoming Number" fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
           <TextField onKeyUp={setTheJobNumber} defaultValue={'AUTO'} label="Job Number" fullWidth InputLabelProps={{ ...InputLabelProps, shrink: true }}/>
           <div className="job-book-review-warranty-and-scrap-text">
-            <div className={`job-book-review-scrap-status ${scrapMap[parseYesNoValues(scrap)].class}`}>{scrapMap[parseYesNoValues(scrap)].text}</div>
             <div className="job-book-review-warranty-text">{(warranty.toUpperCase()==="Y" || warranty.toUpperCase()==="YES")? "Warranty" : ""}</div>
           </div>
         </div>
