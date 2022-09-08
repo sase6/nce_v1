e11cdcd72c8b2679dbdbf199f601dcece95d7cc0
sase <bs.saase@gmail.com>
2022-09-08 14:53:21 -0400

    linked lead tests to state

--- src/desktop-components/paperworks/ProcedureToQualifyingStatorIronsForRewinding.jsx
+++ src/desktop-components/paperworks/ProcedureToQualifyingStatorIronsForRewinding.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
-const { useState, useEffect } = require('react');
+const { useState } = require('react');
 const sheetName = "procedure-to-qualifying-stator-irons-for-rewinding";
-const { TextField, FormControl, InputLabel, Select, MenuItem } = require('@mui/material');
+const { TextField } = require('@mui/material');
 const VisualInspection = require('./subcomponents/ProcedureToQualifyingStatorIronsForRewindingVisualInspection.jsx');
 const RotorQuestions = require('./subcomponents/ProcedureToQualifyingStatorIronsForRewindingRotorQuestions.jsx');
 const Meta = require('./subcomponents/ProcedureToQualifyingStatorIronsForRewindingMeta.jsx');
@@ -29,10 +29,26 @@ module.exports = (props) => {
   const [megOhm3, setMegOhm3] = useState(null);
   const [act44, setAct44] = useState(null);
   const [act14, setAct14] = useState(null);
+  const [s1Sensors, setS1Sensors] = useState(null);
+  const [s2Sensors, setS2Sensors] = useState(null);
+  const [s3Sensors, setS3Sensors] = useState(null);
+  const [byThermal, setByThermal] = useState(null);
+  const [acrossSensor, setAcrossSensor] = useState(null);
+  const [lead3_1, setLead3_1] = useState(null);
+  const [lead3_2, setLead3_2] = useState(null);
+  const [lead3_3, setLead3_3] = useState(null);
+  const [lead6_1, setLead6_1] = useState(null);
+  const [lead6_2, setLead6_2] = useState(null);
+  const [lead6_3, setLead6_3] = useState(null);
+  const [lead9_440_1, setLead9_440_1] = useState(null);
+  const [lead9_440_2, setLead9_440_2] = useState(null);
+  const [lead9_440_3, setLead9_440_3] = useState(null);
+  const [lead9_208_1, setLead9_208_1] = useState(null);
+  const [lead9_208_2, setLead9_208_2] = useState(null);
+  const [lead9_208_3, setLead9_208_3] = useState(null);
 
   return (
     <div className="procedure-to-qualifying-stator-for-rewinding">
-      {/* <div className={`${sheetName}`}></div> */}
       <div className={`${sheetName}-text`}>PROCEDURE TO QUALIFYING STATOR IRONS FOR REWINDING</div>
       <div className={`${sheetName}-sheet`}>
 
@@ -109,7 +125,36 @@ module.exports = (props) => {
         </div>
       </div>
 
-      <LeadTesting sheetName={sheetName}/>
+      <LeadTesting 
+        sheetName={sheetName}
+        lead3_1={lead3_1}
+        setLead3_1={setLead3_1}
+        lead3_2={lead3_2}
+        setLead3_2={setLead3_2}
+        lead3_3={lead3_3}
+        setLead3_3={setLead3_3}
+
+        lead6_1={lead6_1}
+        setLead6_1={setLead6_1}
+        lead6_2={lead6_2}
+        setLead6_2={setLead6_2}
+        lead6_3={lead6_3}
+        setLead6_3={setLead6_3}
+
+        lead9_440_1={lead9_440_1}
+        lead9_440_2={lead9_440_2}
+        lead9_440_3={lead9_440_3}
+        setLead9_440_1={setLead9_440_1}
+        setLead9_440_2={setLead9_440_2}
+        setLead9_440_3={setLead9_440_3}
+
+        lead9_208_1={lead9_208_1}
+        lead9_208_2={lead9_208_2}
+        lead9_208_3={lead9_208_3}
+        setLead9_208_1={setLead9_208_1}
+        setLead9_208_2={setLead9_208_2}
+        setLead9_208_3={setLead9_208_3}
+      />
 
       <div className={`${sheetName}-meg-ohm-leads`}>
         <TextField defaultValue={megOhm1} onChange={(e) => setMegOhm1(e.target.value)} variant="standard" label="Meg-ohm Leads #1: " sx={{width: '250px'}}/>
@@ -134,16 +179,15 @@ module.exports = (props) => {
         <FailedText value={act14} text="NOT APPROVED"/>
       </div>
 
-      {/* Motor Protector */}
       <div className={`${sheetName}-motor-protector-text`}>MOTOR PROTECTOR TERMINALS</div>
       <div className={`${sheetName}-meg-ohm-leads`}>
-        <TextField variant="standard" label="SENSORS COMMON TO #S1: " sx={{width: '250px'}}/>
-        <TextField variant="standard" label="#S2: " sx={{width: '250px'}}/>
-        <TextField variant="standard" label="#S3: " sx={{width: '250px'}}/>
+        <TextField defaultValue={s1Sensors} variant="standard" label="SENSORS COMMON TO #S1: " sx={{width: '250px'}} onChange={(e) => setS1Sensors(e.target.value.toUpperCase())}/>
+        <TextField defaultValue={s2Sensors} variant="standard" label="#S2: " sx={{width: '250px'}} onChange={(e) => setS2Sensors(e.target.value.toUpperCase())}/>
+        <TextField defaultValue={s3Sensors} variant="standard" label="#S3: " sx={{width: '250px'}} onChange={(e) => setS3Sensors(e.target.value.toUpperCase())}/>
       </div>
       <div className={`${sheetName}-motor-protector-ending-questions`}>
-        <TextField variant="standard" label="By Thermal: " sx={{width: '250px'}}/>
-        <TextField variant="standard" label="Across Sensor: " sx={{width: '250px'}}/>
+        <TextField defaultValue={byThermal} variant="standard" label="By Thermal: " sx={{width: '250px'}} onChange={(e) => setByThermal(e.target.value.toUpperCase())}/>
+        <TextField defaultValue={acrossSensor} variant="standard" label="Across Sensor: " sx={{width: '250px'}} onChange={(e) => setAcrossSensor(e.target.value.toUpperCase())}/>
       </div>
 
       </div>
--- src/desktop-components/paperworks/subcomponents/ProcedureToQualifyingStatorIronsForRewindingLeadTesting.jsx
+++ src/desktop-components/paperworks/subcomponents/ProcedureToQualifyingStatorIronsForRewindingLeadTesting.jsx
@@ -1,7 +1,32 @@
 const React = require('react');
 const { TextField } = require('@mui/material');
 
-module.exports = ({sheetName}) => {
+module.exports = ({
+  sheetName,
+  lead3_1,
+  setLead3_1,
+  lead3_2,
+  setLead3_2,
+  lead3_3,
+  setLead3_3,
+  lead6_1,
+  setLead6_1,
+  lead6_2,
+  setLead6_2,
+  lead6_3,
+  setLead6_3,
+  lead9_440_1,
+  lead9_440_2,
+  lead9_440_3,
+  lead9_208_1,
+  lead9_208_2,
+  lead9_208_3,
+  setLead9_440_1,
+  setLead9_440_2,
+  setLead9_440_3,
+  setLead9_208_1,
+  setLead9_208_2,
+  setLead9_208_3}) => {
   return (
     <div className={`${sheetName}-lead-testing`}>
 
@@ -11,6 +36,12 @@ module.exports = ({sheetName}) => {
       t1="LEAD # 1-2"
       t2="LEAD # 2-3"
       t3="LEAD # 3-1"
+      f1={lead3_1}
+      setF1={setLead3_1}
+      f2={lead3_2}
+      setF2={setLead3_2}
+      f3={lead3_3}
+      setF3={setLead3_3}
     />
 
     <LeadTest 
@@ -22,6 +53,12 @@ module.exports = ({sheetName}) => {
       t4="7-8"
       t5="8-9"
       t6="9-7"
+      f1={lead6_1}
+      setF1={setLead6_1}
+      f2={lead6_2}
+      setF2={setLead6_2}
+      f3={lead6_3}
+      setF3={setLead6_3}
     />
 
     <LeadTest 
@@ -33,6 +70,12 @@ module.exports = ({sheetName}) => {
       t4="7-4"
       t5="8-5"
       t6="9-6"
+      f1={lead9_440_1}
+      f2={lead9_440_2}
+      f3={lead9_440_3}
+      setF1={setLead9_440_1}
+      setF2={setLead9_440_2}
+      setF3={setLead9_440_3}
     />
 
     <LeadTest 
@@ -44,6 +87,12 @@ module.exports = ({sheetName}) => {
       t4="4"
       t5="5"
       t6="6"
+      f1={lead9_208_1}
+      f2={lead9_208_2}
+      f3={lead9_208_3}
+      setF1={setLead9_208_1}
+      setF2={setLead9_208_2}
+      setF3={setLead9_208_3}
     />
   </div>
   );
@@ -57,7 +106,7 @@ const LeadTest = ({header, sheetName, t1,t2,t3,t4,t5,t6, f1, f2, f3, setF1, setF
       <div className={`${sheetName}-lead-number-field`}>
         <div className={`${sheetName}-lead-number-field-text`}>{t1}</div>
         <div className={`${sheetName}-lead-number-field-input`}> 
-          <TextField variant='standard' /> 
+          <TextField variant='standard' defaultValue={f1} onChange={(e) => setF1(e.target.value.toUpperCase())}/> 
         </div>
         <div className={`${sheetName}-lead-number-field-text`}>{t4}</div>
       </div>
@@ -65,7 +114,7 @@ const LeadTest = ({header, sheetName, t1,t2,t3,t4,t5,t6, f1, f2, f3, setF1, setF
       <div className={`${sheetName}-lead-number-field`}>
         <div className={`${sheetName}-lead-number-field-text`}>{t2}</div>
         <div className={`${sheetName}-lead-number-field-input`}> 
-          <TextField variant='standard' /> 
+          <TextField variant='standard' defaultValue={f2} onChange={(e) => setF2(e.target.value.toUpperCase())}/> 
         </div>
         <div className={`${sheetName}-lead-number-field-text`}>{t5}</div>
       </div>
@@ -73,7 +122,7 @@ const LeadTest = ({header, sheetName, t1,t2,t3,t4,t5,t6, f1, f2, f3, setF1, setF
       <div className={`${sheetName}-lead-number-field`}>
         <div className={`${sheetName}-lead-number-field-text`}>{t3}</div>
         <div className={`${sheetName}-lead-number-field-input`}> 
-          <TextField variant='standard' /> 
+          <TextField variant='standard' defaultValue={f3} onChange={(e) => setF3(e.target.value.toUpperCase())}/> 
         </div>
         <div className={`${sheetName}-lead-number-field-text`}>{t6}</div>
       </div>
