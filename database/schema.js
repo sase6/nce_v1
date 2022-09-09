deb806ea50245342476c40d3f08a77540e1145a7
sase <bs.saase@gmail.com>
2022-09-09 09:07:47 -0400

    created p1 schema

--- database/schema.js
+++ database/schema.js
@@ -32,6 +32,50 @@ const jobSchema = new mongoose.Schema({
 
 const Job = new mongoose.model('job', jobSchema);
 
+const procedureToQualifyingStatorIronsForRewindingSchema = new mongoose.Schema({
+  modelNumber: String,
+  jobNumber: String,
+  voltage: String,
+  leads: String,
+  imp: String,
+  phase: String,
+  rotorFitShaft: Boolean,
+  rotorMatchShaft: Boolean,
+  ironDmgTest: Boolean,
+  coreLossTest: Boolean,
+  hotSpotTest: Boolean,
+  isRewinding: String,
+  rotorNumber: String,
+  whoIsWinding: String,
+  megOhm1: String,
+  megOhm2: String,
+  megOhm3: String,
+  act44: Boolean,
+  act14: Boolean,
+  s1Sensors: String,
+  s2Sensors: String,
+  s3Sensors: String,
+  byThermal: String,
+  acrossSensor: String,
+  lead3_1: String,
+  lead3_2: String,
+  lead3_3: String,
+  lead6_1: String,
+  lead6_2: String,
+  lead6_3: String,
+  lead9_440_1: String,
+  lead9_440_2: String,
+  lead9_440_3: String,
+  lead9_208_1: String,
+  lead9_208_2: String,
+  lead9_208_3: String,
+});
+
+const P1 = new mongoose.model('P1', procedureToQualifyingStatorIronsForRewindingSchema);
+
 module.exports = {
-  User, Job
+  User, Job, P1
 };
+
+//Key:
+// P1 -> procedureToQualifyingStatorIronsForRewinding 
