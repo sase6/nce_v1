471fe12d6f4be3118c519e8da9d386058f3ffe08
sase <bs.saase@gmail.com>
2022-09-26 15:16:08 -0400

    complete bartender integration

--- src/desktop-components/barcode-bartender/P2Meta.jsx
+++ src/desktop-components/barcode-bartender/P2Meta.jsx
@@ -1,7 +1,25 @@
 const React = require('react');
+const {useEffect} = require('react');
 const bar128 = require('./bar128.js');
 
-module.exports = ({}) => {
+module.exports = ({serialNumber="43892", partNumber="", revision="", description="", setSerialNumber, setPartNumber, setRevision, setDescription}) => {
+
+  useEffect(() => {
+    document.querySelector('#serialNumber').value = serialNumber;
+    document.querySelector('#partNumber').value = partNumber;
+    document.querySelector('#revision').value = revision;
+    document.querySelector('#description').value = description;
+  }, [serialNumber, partNumber, revision, description]);
+
+  const setValue = (e) => {
+    const type = e.target.id;
+    const val = e.target.value;
+    if (type === 'serialNumber') setSerialNumber(val);
+    else if (type === 'partNumber') setPartNumber(val);
+    else if (type === 'revision') setRevision(val);
+    else if (type === 'description') setDescription(val);
+  };
+
   return (
     <div className="p2-part-and-description">
       <div className="p2-part-and-description-info">
@@ -10,20 +28,28 @@ module.exports = ({}) => {
           <div className="p2-part-order-number-header p2-header">ORDER NUMBER</div>
           <div className="p2-part-part-number-header p2-header">PART NUMBER</div>
           <div className="p2-part-revision-header p2-header">REVISION</div>
-          <div className="p2-part-order-number-text">{'serialNumber'}</div>
-          <div className="p2-part-part-number-text">{'partNumber'}</div>
-          <div className="p2-part-revision-text">{'revision'}</div>
+          <div className="p2-part-order-number-text">
+            <input type="text" id="serialNumber" className='p2-input' defaultValue={serialNumber} onChange={setValue}/>
+          </div>
+          <div className="p2-part-part-number-text">
+            <input type="text" id="partNumber" className='p2-input' defaultValue={partNumber} onChange={setValue}/>
+          </div>
+          <div className="p2-part-revision-text">
+            <input type="text" id="revision" className='p2-input' defaultValue={revision} onChange={setValue}/>
+          </div>
         </div>
      
         <div className="p2-part-description">
           <div className="p2-part-description-header p2-header">DESCRIPTION</div>
-          <div className="p2-part-description-text">{'description'}</div>
+          <div className="p2-part-description-text">
+            <input type="text" id="description" className='p2-input' defaultValue={description} onChange={setValue}/>
+          </div>
         </div>
       </div>
 
       <div className="p2-part-and-description-barcode">
-        <div className="p2-job-number-barcode bar128">{bar128('70106')}</div>
-        <div className="p2-job-number-text">70106</div>
+        <div className="p2-job-number-barcode bar128">{bar128(serialNumber)}</div>
+        <div className="p2-job-number-text">{serialNumber}</div>
       </div>
     </div>
   );
--- src/desktop-components/barcode-bartender/ShopFloorProcessSheet.jsx
+++ src/desktop-components/barcode-bartender/ShopFloorProcessSheet.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 const {FormGroup, FormControlLabel, Checkbox, TextField, InputLabelProps} = require('@mui/material');
 const P2Meta = require('./P2Meta.jsx');
 
-module.exports = ({sheet, modelNumber, serialNumber, voltage, partNumber, revision, description, stock, oil, customerName, discus, delta, resistorKit}) => {
+module.exports = ({sheet, modelNumber, serialNumber, voltage, partNumber, revision, description, stock, oil, customerName, discus, delta, dxs, setSerialNumber, setPartNumber, setRevision, setDescription, setStock, setOil, setCustomerName, setDiscus, setDelta, setDxs}) => {
     if (sheet!== 'paperwork' && sheet !== 'both') return;
     
     return (
@@ -10,60 +10,91 @@ module.exports = ({sheet, modelNumber, serialNumber, voltage, partNumber, revisi
       <div className="shop-floor-process-sheet-upper">
         <div className="shop-floor-process-sheet-title">SHOP FLOOR PROCESS SHEET</div>
 
-        <P2Meta />
+        <P2Meta
+          serialNumber={serialNumber}
+          partNumber={partNumber}
+          revision={revision}
+          description={description}
+          setSerialNumber={setSerialNumber}
+          setPartNumber={setPartNumber}
+          setRevision={setRevision}
+          setDescription={setDescription}
+        />
 
         <div className="p2-customer-info-container">
           <div className="p2-customer-info-boxes">
             <div className="stock-checkbox">
               <FormGroup>
-                <FormControlLabel control={<Checkbox defaultChecked disableRipple sx={{ '& .MuiSvgIcon-root': { fontSize: 30 } }}/>} label="Stock" />
+                <FormControlLabel sx={{color: 'black'}} control={<Checkbox defaultChecked={stock} onChange={(e) => setStock(e.target.checked)} disableRipple sx={{ '& .MuiSvgIcon-root': { fontSize: 30, color: 'black' } }}/>} label="Stock" />
               </FormGroup>
             </div>
 
             <div className="p2-oil-boxes">
             <div className="oil-checkbox">
               <FormGroup>
-                <FormControlLabel control={<Checkbox defaultChecked disableRipple sx={{ '& .MuiSvgIcon-root': { fontSize: 30 } }}/>} label="Oil" />
+                <FormControlLabel sx={{color: 'black'}} control={<Checkbox defaultChecked={oil} onChange={(e) => setOil(e.target.checked)} disableRipple sx={{ '& .MuiSvgIcon-root': { fontSize: 30, color: 'black' } }}/>} label="Oil" />
               </FormGroup>
             </div>
             <div className="no-oil-checkbox">
               <FormGroup>
-                <FormControlLabel control={<Checkbox defaultChecked disableRipple sx={{ '& .MuiSvgIcon-root': { fontSize: 30 } }}/>} label="No-Oil" />
+                <FormControlLabel sx={{color: 'black'}} control={<Checkbox defaultChecked={!oil} onChange={(e) => setOil(!e.target.checked)}  disableRipple sx={{ '& .MuiSvgIcon-root': { fontSize: 30, color: 'black' } }}/>} label="No-Oil" />
               </FormGroup>
             </div>
             </div>
 
           </div>
 
-          
+
           <div className="p2-customer-name-container">
             <div className="customer-name-input">
-              <TextField InputLabelProps={{shrink: true}} variant='standard' fullWidth size='small' sx={{height: 50}} label="Customer Name"/>
+              <TextField InputLabelProps={{shrink: true}} defaultValue={customerName} onChange={(e) => setCustomerName(e.target.value)} variant='standard' fullWidth size='small' sx={{height: 50, color: 'black'}} label="Customer Name"/>
             </div>
           </div>
         </div>
 
-        {/* <div className="p2-copeland-info-box">
+        <div className="p2-copeland-info-box">
           <div className="copeland-text">COPELAND: </div>
-          <div className="discus-checkbox">[] Discus</div>
-          <div className="delta-checkbox">[] Delta</div>
-          <div className="delta-post-text">IF DELTA, MAKE SURE AN "A" IS ADDED TO MODEL# (4DK3A)</div>
+          <div className="discus-checkbox">
+            <div className="discus-checkbox">
+              <FormGroup>
+                <FormControlLabel control={<Checkbox disableRipple defaultChecked={discus} onChange={(e) => setDiscus(e.target.checked)} sx={{ '& .MuiSvgIcon-root': { fontSize: 30, color: 'black' } }}/>} label="Discus" />
+              </FormGroup>
+            </div>
+          </div>
+          <div className="delta-checkbox">
+            <div className="delta-checkbox">
+              <FormGroup>
+                <FormControlLabel control={<Checkbox disableRipple defaultChecked={delta} onChange={(e) => setDelta(e.target.checked)} sx={{ '& .MuiSvgIcon-root': { fontSize: 30, color: 'black' } }}/>} label="Delta" />
+              </FormGroup>
+            </div>
+          </div>
+          <div className="delta-post-text">If Delta, make sure an "A" is added to the model # (Ex: 4DK3A)</div>
         </div>
 
         <div className="p2-dxs-checkbox">
           <div className="p2-dsx-text">DXS YORK SCREWS - IS RESISTOR KIT NEEDED: </div>
-          <div className="yes-checkbox">[] YES</div>
-          <div className="no-checkbox">[] NO</div>
-        </div> */}
+          <div className="yes-checkbox">
+            <div className="yes-checkbox">
+              <FormGroup>
+                <FormControlLabel control={<Checkbox disableRipple defaultChecked={dxs} onChange={(e) => setDxs(e.target.checked)} sx={{ '& .MuiSvgIcon-root': { fontSize: 30, color: 'black' } }}/>} label="Yes" />
+              </FormGroup>
+            </div>
+          </div>
+          <div className="no-checkbox">
+            <div className="no-checkbox">
+              <FormGroup>
+                <FormControlLabel control={<Checkbox disableRipple defaultChecked={!dxs} onChange={(e) => setDxs(!e.target.checked)} sx={{ '& .MuiSvgIcon-root': { fontSize: 30, color: 'black' } }}/>} label="No" />
+              </FormGroup>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="shop-floor-process-sheet-lower">
-        
         <div className="p2-meta-info-container">
           <div className="p2-meta-model-num">{modelNumber}</div>
           <div className="p2-meta-other-vals">{`S#${serialNumber} ${voltage}V`}</div>
         </div>
-      
       </div>
     </div>
   );
